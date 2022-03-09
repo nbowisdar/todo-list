f4c22781a500e2dc853322ac13891e668ceebf48
Vova <johndoe@example.com>
2022-03-09 13:29:01 +0200

    registration

--- todo/urls.py
+++ todo/urls.py
@@ -6,8 +6,6 @@ from todo import views
 
 
 urlpatterns = [
-    #path('', first, name='home'),
-    #path('login', views.login, name='login'),
     path('', views.ShowList.as_view(), name='tasks'),
     path('create/', views.CreateTask.as_view(), name='create'),
     path('update-task/<int:pk>', views.UpdateTask.as_view(), name='update'),
@@ -15,6 +13,6 @@ urlpatterns = [
     path('task/<int:pk>/', views.ShowTask.as_view()),
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(template_name='todo/reg/logout.html'), name='logout'),
-    path('registration/', views.registration, name='register'),
-    path('test-redirect/', views.register_request)
+    path('registration/', views.RegisterUser.as_view(), name='register'),
+    path('info', views.show_info)
 ]
--- todo/views.py
+++ todo/views.py
@@ -9,65 +9,40 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import CreateView, DeleteView, DetailView, CreateView, UpdateView
+from django.views.generic import FormView, DeleteView, DetailView, CreateView, UpdateView
 from django.views.generic.list import ListView
 from .models import Task
-#from django.views.generic.edit import CreateView, UpdateView
 from django.contrib import messages
-
-def register_request(request):
-	if request.method == "POST":
-		form = UserCreationForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("main:homepage")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = UserCreationForm()
-	return render (request=request, template_name="todo/reg/register.html", context={"form":form})
 
 class LoginUser(LoginView):
     template_name = 'todo/reg/login.html'
-    #success_url = reverse_lazy('tasks')
-    #next_page = ''
+    extra_context = {'title':'login'}
+    redirect_authenticated_user = True
 
-def registration(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('tasks')
-    else:
-        form = UserCreationForm()
-        return render(request, 'todo/reg/register.html', {'title':'registration', 'form':form})
+class RegisterUser(FormView):
+    template_name = 'todo/reg/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('tasks')
+    extra_context = 'registration'
+    redirect_authenticated_user = True
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterUser, self).form_valid(form)
 
 def test_rediretc(request):
     return redirect('tasks')
-
-def register(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            user = authenticate(form.cleaned_data)
-            #print(form.cleaned_data['username'])
-            x = LoginView()
-    form = LoginUser()
-
-    return render(request, 'todo/test.html', {'form':form})
-
-def test(request, id):
-    return HttpResponse(f'Hello, your number is - {id}')
 
 class CreateTask(CreateView):
     model = Task
     form_class = TaskForm
     template_name = 'todo/create_task.html'
     extra_context = {'title':'create-task'}
-    # def get_context_data(self, **kwargs):
-    #     context = super(CreateTask, self).get_context_data()
-    #     context['title'] = 'create-task'
-    #     return context
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(CreateTask, self).form_valid(form)
 
 class UpdateTask(UpdateView):
     model = Task
@@ -75,14 +50,18 @@ class UpdateTask(UpdateView):
     template_name = 'todo/create_task.html'
     extra_context = {'title':'update-task'}
 
-
-
 class ShowList(ListView):
     model = Task
     template_name = 'todo/show_list.html'
     context_object_name = 'tasks'
     extra_context = {'title':'all-tasks'}
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ShowList, self).get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        return context
 
+def show_info(request):
+    return HttpResponse(f'<h1>{request.user}</h1>')
 
 class ShowTask(DetailView):
     model = Task
@@ -94,13 +73,10 @@ class ShowTask(DetailView):
         context['title'] = self.kwargs['pk']
         return context
 
-
 class DeleteTask(DeleteView):
     model = Task
     template_name = 'todo/show_task.html'
     content_object_name = 'task'
     success_url = reverse_lazy('tasks')
-    #def delete(self, request, *args, **kwargs):
-
     def get(self, request, *args, **kwargs):
         return super(DeleteTask, self).delete(request, *args, **kwargs)
